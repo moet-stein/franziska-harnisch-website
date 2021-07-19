b7823843a65d7e2417734c09bf699a741df40663
Moe Steinmueller <0101.mste@gmail.com>
2021-07-19 15:57:29 +0200

    fixed testMoe

--- src/cms/cms.tsx
+++ src/cms/cms.tsx
@@ -1,19 +1,22 @@
-import CMS from "netlify-cms-app";
-import uploadcare from "netlify-cms-media-library-uploadcare";
-import cloudinary from "netlify-cms-media-library-cloudinary";
+import CMS from 'netlify-cms-app';
+import uploadcare from 'netlify-cms-media-library-uploadcare';
+import cloudinary from 'netlify-cms-media-library-cloudinary';
 
-import AboutPagePreview from "./preview-templates/AboutPagePreview";
-import BlogPostPreview from "./preview-templates/BlogPostPreview";
-import ProductPagePreview from "./preview-templates/ProductPagePreview";
-import IndexPagePreview from "./preview-templates/IndexPagePreview";
-import EventPagePreview from "./preview-templates/EventPagePreview";
+import AboutPagePreview from './preview-templates/AboutPagePreview';
+import BlogPostPreview from './preview-templates/BlogPostPreview';
+import ProductPagePreview from './preview-templates/ProductPagePreview';
+import IndexPagePreview from './preview-templates/IndexPagePreview';
+import EventPagePreview from './preview-templates/EventPagePreview';
+import LauraTestPreview from './preview-templates/LauraTestPreview';
+import TestMoePagePreview from './preview-templates/TestMoePagePreview';
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
 
-CMS.registerPreviewTemplate("index", IndexPagePreview);
-CMS.registerPreviewTemplate("about", AboutPagePreview);
-CMS.registerPreviewTemplate("products", ProductPagePreview);
-CMS.registerPreviewTemplate("blog", BlogPostPreview);
-CMS.registerPreviewTemplate("events", EventPagePreview);
-CMS.registerPreviewTemplate("lauratest", LauraTestPreview);
+CMS.registerPreviewTemplate('index', IndexPagePreview);
+CMS.registerPreviewTemplate('about', AboutPagePreview);
+CMS.registerPreviewTemplate('products', ProductPagePreview);
+CMS.registerPreviewTemplate('blog', BlogPostPreview);
+CMS.registerPreviewTemplate('events', EventPagePreview);
+CMS.registerPreviewTemplate('lauratest', LauraTestPreview);
+CMS.registerPreviewTemplate('testMoe', TestMoePagePreview);
