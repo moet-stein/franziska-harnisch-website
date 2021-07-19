d26181d8c636be4a484ad643e381530487fdd74c
Laura <65158950+lauratro@users.noreply.github.com>
2021-07-19 13:01:16 +0200

    Laura Test files 2. part

---
+++ src/cms/preview-templates/LauraTestPreview.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { LauraTestTemplate } from '../../templates/lauraTest-page'
+
+const LauraTestPreview = ({ entry, getAsset }) => {
+  const data = entry.getIn(['data']).toJS()
+
+  if (data) {
+    return (
+      <LauraTestTemplate
+        image={getAsset(data.image)}
+        title={data.title}
+        heading={data.heading}
+        subheading={data.subheading}
+        description={data.description}
+        intro={data.intro || { blurbs: [] }}
+        mainpitch={data.mainpitch || {}}
+      />
+    )
+  } else {
+    return <div>Loading...</div>
+  }
+}
+
+LauraTestPreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  getAsset: PropTypes.func,
+}
+
+export default LauraTestPreview
