420f42bc31e4b86487fef919f7d2ff917432ad5f
Moe Steinmueller <0101.mste@gmail.com>
2021-07-21 11:19:42 +0200

    plugin material ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sass",
     "gatsby-plugin-typescript",
+    "gatsby-plugin-material-ui",
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: "gatsby-source-filesystem",
