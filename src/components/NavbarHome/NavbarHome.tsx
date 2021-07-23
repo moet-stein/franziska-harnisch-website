cf393c123ff845991c0e7cf205b4697fdfd88ae4
Laura <65158950+lauratro@users.noreply.github.com>
2021-07-23 12:03:01 +0200

    change color effect mouse over menu

--- src/components/NavbarHome/NavbarHome.tsx
+++ src/components/NavbarHome/NavbarHome.tsx
@@ -1,4 +1,4 @@
-import React, {useRef, useState, useEffect} from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { Link } from "gatsby";
 import ListIcon from '@material-ui/icons/List';
 import "./NavBarHomeStyle.css"
@@ -6,7 +6,7 @@ import "./NavBarHomeStyle.css"
 //import logo from "../img/logo.svg";
 
 function NavbarHome() {
-   const dropdownRef = useRef(null);
+  const dropdownRef = useRef(null);
   const [isActive, setIsActive] = useState(false);
   const onClick = () => setIsActive(!isActive);
   /* constructor(props) {
@@ -37,19 +37,19 @@ function NavbarHome() {
     );
   };
  */
- // render() {
-    return (
-      <nav
-        
-            className="posFixed navdimensions nav-container"
-            role="navigation"
-        aria-label="main-navigation"
-      >
-        <div className="dispFlexRow dfspacearound navMenu">
-          <div className="navbar-brand">
-          
-            {/* Hamburger menu */}
-           {/*  <div
+  // render() {
+  return (
+    <nav
+
+      className="posFixed navdimensions nav-container"
+      role="navigation"
+      aria-label="main-navigation"
+    >
+      <div className="dispFlexRow dfspacearound navMenu">
+        <div className="navbar-brand">
+
+          {/* Hamburger menu */}
+          {/*  <div
               className={`navbar-burger burger ${this.state.navBarActiveClass}`}
               data-target="navMenu"
               onClick={() => this.toggleHamburger()}
@@ -58,43 +58,43 @@ function NavbarHome() {
               <span />
               <span />
             </div> */}
-          </div>
-          <div
-            id="navMenu"
-           // className={`navbar-menu ${this.state.navBarActiveClass}`}
-          >
-            <div className="dispflexrow dfspacearound"  >
-              <Link className="navbar-item" to="/about">
-             About
-              </Link>
-              <Link className="navbar-item" to="/testMoe">
+        </div>
+        <div
+          id="navMenu"
+        // className={`navbar-menu ${this.state.navBarActiveClass}`}
+        >
+          <div className="dispflexrow dfspacearound"  >
+            <Link className="navbar-item" to="/hometwo">
+              About
+            </Link>
+            <Link className="navbar-item" to="/testMoe">
               Works
-              </Link>
-              <Link className="navbar-item" to="/">
-             Exibitions
-              </Link>
-              <Link className="navbar-item" to="/">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/">
-                Links
-              </Link>
-               <div className="menu-container" style={{display:"inline-block"}}>
+            </Link>
+            <Link className="navbar-item" to="/">
+              Exibitions
+            </Link>
+            <Link className="navbar-item" to="/">
+              Contact
+            </Link>
+            <Link className="navbar-item" to="/">
+              Links
+            </Link>
+            <div className="menu-container" style={{ display: "inline-block" }}>
               <p onClick={onClick} className="menu-trigger navbar-item" >
-              <ListIcon/>
-             
-                </p>
-                </div>
-              <nav ref={dropdownRef} className={`menu ${isActive ? 'active' : 'inactive'}`}>
-        <ul>
-          
-        <li><a href="">Impressum</a></li>
-      <li><a href="">Datenschutz</a></li>
-        </ul>
-              </nav>
-               </div>
-            
-            <div className="navbar-end has-text-centered">
+                <ListIcon />
+
+              </p>
+            </div>
+            <nav ref={dropdownRef} className={`menu ${isActive ? 'active' : 'inactive'}`}>
+              <ul>
+
+                <li><a href="">Impressum</a></li>
+                <li><a href="">Datenschutz</a></li>
+              </ul>
+            </nav>
+          </div>
+
+          <div className="navbar-end has-text-centered">
             {/*   <a
                 className="navbar-item"
                 href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
@@ -105,12 +105,12 @@ function NavbarHome() {
                   <img src={github} alt="Github" />
                 </span> 
               </a> */}
-            </div>
           </div>
         </div>
-      </nav>
-    );
- // }
+      </div>
+    </nav>
+  );
+  // }
 };
 
 export default NavbarHome;
