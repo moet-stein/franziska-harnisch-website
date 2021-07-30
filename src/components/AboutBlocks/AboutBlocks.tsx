57a42680d4833f5a7e5571e8ef7cca9494b382a3
Laura <65158950+lauratro@users.noreply.github.com>
2021-07-30 10:07:17 +0200

    about blocks animation added

--- src/components/AboutBlocks/AboutBlocks.tsx
+++ src/components/AboutBlocks/AboutBlocks.tsx
@@ -12,47 +12,66 @@ const useStyles = makeStyles(theme => ({
         maxWidth: "fit-content",
         textAlign: "center",
         padding: 15,
-        margin:10
-    }
+        margin: 3,
+        
+    },
+    animatedItem: {
+        animation: `$myEffect 2000ms ${theme.transitions.easing.easeInOut}`
+    },
+        "@keyframes myEffect": {
+        "0%": {
+            opacity: 0,
+
+        },
+        "100%": {
+            opacity: 1,
+
+        }
+    },
+    animatedItem2: {
+        animation: `$myEffect 4000ms ${theme.transitions.easing.easeInOut}`
+    },
+       
+
 }))
 
 export default function AboutBlocks({ generalInfo, ausbildung, preise, einzelaustellung, gruppenaustellung, projekte }) {
     const classes = useStyles();
 
-    return (<Grid container className={classes.displayFlexCenter}>
-        <Grid item xs={12} className={classes.borderBlock}>
+    return (<Grid container spacing={1} className={classes.displayFlexCenter} >
+        <Grid item xs={12} spacing={3} className={`${classes.borderBlock} ${classes.animatedItem}`}>
             <h3>{generalInfo.name}</h3>
             <h4>{generalInfo.address}</h4>
             <h4>{generalInfo.website}</h4>
             <h4>{generalInfo.email}</h4>
         </Grid>
 
-        <Grid container>
-            <Grid xs={6} item className={classes.borderBlock}>
+        <Grid container className={classes.animatedItem2}>
+            <Grid item xs={12} sm={6} spacing={3}  className={classes.borderBlock}>
                 <h3>{ausbildung.title}</h3>
                 {ausbildung.texts.map(a => {
                     return <p>{a.text}</p>
                 })}
             </Grid>
-            <Grid xs={6} item className={classes.borderBlock}>
+            <Grid xs={12} sm={6} item className={classes.borderBlock}>
                 <h3>{preise.title}</h3>
                 {preise.texts.map(a => {
                     return <p>{a.text}</p>
                 })}
             </Grid>
-            <Grid xs={6} item className={classes.borderBlock}>
+            <Grid xs={12} sm={6} item className={classes.borderBlock}>
                 <h3>{einzelaustellung.title}</h3>
                 {einzelaustellung.texts.map(a => {
                     return <p>{a.text}</p>
                 })}
             </Grid>
-            <Grid xs={6} item className={classes.borderBlock}>
+            <Grid xs={12} sm={6} item className={classes.borderBlock}>
                 <h3>{gruppenaustellung.title}</h3>
                 {gruppenaustellung.texts.map(a => {
                     return <p>{a.text}</p>
                 })}
             </Grid>
-            <Grid xs={6} item className={classes.borderBlock}>
+            <Grid xs={12} sm={6} item className={classes.borderBlock}>
                 <h3>{projekte.title}</h3>
                 {projekte.texts.map(a => {
                     return <p>{a.text}</p>
