6f7b7e3966ef16a4446e165b719f768f2a040acb
Moe Steinmueller <0101.mste@gmail.com>
2021-07-30 17:05:41 +0200

    worked on exhibitions

--- src/components/Exhibitions/Exhibitions.tsx
+++ src/components/Exhibitions/Exhibitions.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 import grey from '@material-ui/core/colors/grey';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -22,12 +23,21 @@ const useStyles = makeStyles(() => ({
   },
   upWidth: {
     width: '400px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imgWidth: {
+    width: '150px',
+    borderRadius: '5px',
   },
 }));
 
 export default function Exhibitions({ upcomingExhibitions, exhibitions }) {
   const classes = useStyles();
+  const [upcomingDate, setUpcomingDate] = useState('');
   console.log(upcomingExhibitions, exhibitions);
+
   return (
     <>
       <Box className={classes.flexColumn}>
@@ -40,9 +50,36 @@ export default function Exhibitions({ upcomingExhibitions, exhibitions }) {
           {upcomingExhibitions.length > 0 &&
             upcomingExhibitions.map((u) => (
               <Box className={classes.upWidth}>
-                <Typography varaint="h6">{u.name}</Typography>
-                {u.date && (
-                  <Typography variant="body2">{u.date.slice(0, 10)}</Typography>
+                <Box>
+                  <Typography varaint="h6">{u.name}</Typography>
+                  <Box display="flex">
+                    {u.startDate && (
+                      <Typography variant="body2">
+                        {u.startDate.slice(0, 10)}
+                      </Typography>
+                    )}{' '}
+                    {u.endDate && (
+                      <Typography variant="body2">
+                        - {u.endDate.slice(0, 10)}
+                      </Typography>
+                    )}
+                  </Box>
+                  {u.place && <Typography>{u.place}</Typography>}
+                  {u.links.length > 0 &&
+                    u.links.map((l) => <Typography>{l.linkURL}</Typography>)}
+                </Box>
+                {u.image && (
+                  <Box m={2}>
+                    <img
+                      className={classes.imgWidth}
+                      src={u.image.childImageSharp.fluid.src}
+                    />
+                  </Box>
+                )}
+                {!u.image && (
+                  <Box m={2}>
+                    <Paper className={classes.imgWidth} />
+                  </Box>
                 )}
               </Box>
             ))}
