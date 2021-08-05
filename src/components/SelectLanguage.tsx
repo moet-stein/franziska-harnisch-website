50a34c6404e701d963aff5fb8dddc0f827b4f70b
Moe Steinmueller <0101.mste@gmail.com>
2021-08-05 11:30:31 +0200

    selectlanguage edited for routes

--- src/components/SelectLanguage.tsx
+++ src/components/SelectLanguage.tsx
@@ -1,23 +1,27 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { navigate } from "gatsby";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import { makeStyles, } from "@material-ui/styles"
-import { fade, } from "@material-ui/core"
+import React from 'react';
+import PropTypes from 'prop-types';
+import { navigate } from 'gatsby';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { makeStyles } from '@material-ui/styles';
+import { fade } from '@material-ui/core';
+import { useLocation } from '@reach/router';
 
 const useStyles = makeStyles((theme) => ({
   list: {
-    backgroundColor: fade(theme.palette.secondary.main, 0.9)
+    backgroundColor: fade(theme.palette.secondary.main, 0.9),
   },
-
-}))
+}));
 
 const SelectLanguage = ({ langs }) => {
   const classes = useStyles();
+  const pathName = useLocation().pathname;
+  console.log(langs);
+  console.log(pathName);
+
   return (
-    <List component="ul" aria-label="languages" style={{ display: "flex" }}>
+    <List component="ul" aria-label="languages" style={{ display: 'flex' }}>
       {langs.map((lang) => (
         <ListItem
           //style={{ color }}
@@ -26,18 +30,19 @@ const SelectLanguage = ({ langs }) => {
           onClick={(event) => {
             event.preventDefault();
             const { langKey } = lang;
-            if (lang.link === "/") navigate(`/`);
-            else if (!lang.link.includes("/de/")) {
-              navigate(`${langKey}/${lang.link}`);
-            } else navigate(`${lang.link}`);
+            if (lang.link == '/') {
+              navigate(`/`);
+            } else if (!lang.link.includes('/en/')) {
+              navigate(`${pathName}`.slice(3));
+            } else {
+              navigate(`/${langKey}${pathName}`);
+            }
           }}
           selected={lang.selected}
         >
-          <ListItemText className={classes.listItem}
-            primary={lang.langKey} />
+          <ListItemText className={classes.listItem} primary={lang.langKey} />
         </ListItem>
-      ))
-      }
+      ))}
     </List>
   );
 };
