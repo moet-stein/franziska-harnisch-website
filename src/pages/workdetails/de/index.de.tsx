bd1bc6efdbcda1a64cb99a374713a1dfb8ebc974
Moe Steinmueller <0101.mste@gmail.com>
2021-08-06 15:48:35 +0200

    testing workdetails

--- src/pages/workdetails/de/index.de.tsx
+++ src/pages/workdetails/de/index.de.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import WorkdetailsRoll from '../../components/WorkdetailsRoll/WorkdetailsRoll';
+import WorkdetailsRoll from '../../../components/WorkdetailsRoll/WorkdetailsRoll';
 
-import Layout from '../../components/Layout';
+import Layout from '../../../components/Layout';
 
 export default function WorkdetailsIndexPage() {
   return (
