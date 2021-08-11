d2cc0ebedf421a2d9f2eef81646ea7e00e8af52f
Moe Steinmueller <0101.mste@gmail.com>
2021-08-11 16:26:09 +0200

    yml pdf changed

--- src/components/LayPdf/LayPdf.tsx
+++ src/components/LayPdf/LayPdf.tsx
@@ -3,7 +3,7 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a lo
 import { Carousel } from 'react-responsive-carousel';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   carSize: {
     width: '800px',
   },
