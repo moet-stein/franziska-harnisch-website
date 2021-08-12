938ca8a16ed9c511254a7978658404991c23a5c4
Moe Steinmueller <0101.mste@gmail.com>
2021-08-12 17:42:14 +0200

    seo edited

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
 const languages = require('./src/data/languages');
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
-    description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
-    languages,
+    title: 'Franziska Harnisch',
+    author: 'Franziska Harnisch',
+    keywords: ['Franziska', 'Harnisch', 'art'],
+    languages
   },
   plugins: [
     'gatsby-plugin-react-helmet',
