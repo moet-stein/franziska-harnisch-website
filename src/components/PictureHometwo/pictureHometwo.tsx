6b4b3f2d05c3c51e8785c95f67a7a40ab36255c8
Laura <65158950+lauratro@users.noreply.github.com>
2021-08-13 15:15:45 +0200

    pictures homepage new layout and display structure

--- src/components/PictureHometwo/pictureHometwo.tsx
+++ src/components/PictureHometwo/pictureHometwo.tsx
@@ -46,6 +46,7 @@ const textWidth = {
 }
 export default function PictureHometwo({ pic }) {
     console.log("pic", pic[0].image)
+    const [newArray, setNewArray] = useState([])
     useEffect(() => {
         setExit(false)
     }, [])
@@ -54,31 +55,51 @@ export default function PictureHometwo({ pic }) {
     const [exit, setExit] = useState(false);
     const [thirdPic, setThirdPic] = useState(false)
 
-    return (<Grid container style={{ width: "100%" }} >
-        <Grid item xs={12} sm={4} style={{ paddingLeft: 30, display:"flex", flexDirection:"column-reverse" }}  >
-            {/*  <PreviewCompatibleImage imageInfo={pic[0].image} style={{ borderRadius: 5, }} onMouseOver={() => setExit(prev => !prev)} className={exit ? classes.imageMask : classes.imageNoMask} image={pic[0].image} fluid={pic[0].image.childImageSharp.fluid.src} /> */}
-            <img style={{ borderRadius: 5, width:240 }} onMouseOver={() => setExit(prev => !prev)} className={exit ? classes.imageMask : classes.imageNoMask} src={typeof pic[0].image === 'string' ? pic[0].image : pic[0].image} />
+    const shuffle = (array) => {
+        var currentIndex = array.length,
+          randomIndex;
+    
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+          // Pick a remaining element...
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+    
+          // And swap it with the current element.
+          [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex],
+            array[currentIndex],
+          ];
+        }
+        return array;
+      };
+
+useEffect(()=>{
+ setNewArray(shuffle(pic))
+ console.log(newArray)
+},[])
+
+console.log("pic", newArray)
+if(newArray.length >0){
+return(
+   <Grid container style={{ width: "100%" }} >
+     <Grid item xs={12} sm={4} style={{ paddingLeft: 30}}  >
+          
+            <img style={{ borderRadius: 5, width:240 }} onMouseOver={() => setExit(prev => !prev)} className={exit ? classes.imageMask : classes.imageNoMask} src={ newArray[0].image} />
             <p className={exit ? classes.animatedItem : classes.animatedItemExiting}
-                style={textWidth} >{pic[0].text}</p>
+                style={textWidth} >{newArray[0].text}</p>
         </Grid>
-        <Grid item xs={12} sm={4} >  {/*             <PreviewCompatibleImage imageInfo={pic[2].image} style={{ borderRadius: 5 }} onMouseOver={() => setVisible(prev => !prev)} className={visible ? classes.imageMask : classes.imageNoMask} image={pic[2].image} fluid={pic[2].image.childImageSharp.fluid.src} /> */}
-            <img style={{ borderRadius: 5, width:240 }} onMouseOver={() => setVisible(prev => !prev)} className={visible ? classes.imageMask : classes.imageNoMask} src={typeof pic[1].image === 'string' ? pic[1].image : pic[1].image} />
-            <p style={textWidth} className={visible ? classes.animatedItem : classes.animatedItemExiting}>{pic[1].text}</p></Grid>
+        <Grid item xs={12} sm={4} > 
+            <img style={{ borderRadius: 5, width:240 }} onMouseOver={() => setVisible(prev => !prev)} className={visible ? classes.imageMask : classes.imageNoMask} src={ newArray[1].image } />
+            <p style={textWidth} className={visible ? classes.animatedItem : classes.animatedItemExiting}>{newArray[1].text}</p></Grid>
         <Grid item xs={12} sm={4}  >
-            {/*             <PreviewCompatibleImage imageInfo={pic[1].image} style={{ borderRadius: 5 }} onMouseOver={() => setThirdPic(prev => !prev)} className={thirdPic ? classes.imageMask : classes.imageNoMask} image={pic[1].image} fluid={pic[1].image.childImageSharp.fluid.src} /> */}
-            <img style={{ borderRadius: 5, width:240 }} onMouseOver={() => setThirdPic(prev => !prev)} className={thirdPic ? classes.imageMask : classes.imageNoMask} src={typeof pic[2].image === 'string' ? pic[2].image : pic[2].image} />
-            <p style={textWidth} className={thirdPic ? classes.animatedItem : classes.animatedItemExiting}>{pic[2].text}</p>
+        
+            <img style={{ borderRadius: 5, width:240 }} onMouseOver={() => setThirdPic(prev => !prev)} className={thirdPic ? classes.imageMask : classes.imageNoMask} src={ newArray[2].image } />
+            <p style={textWidth} className={thirdPic ? classes.animatedItem : classes.animatedItemExiting}>{newArray[2].text}</p>
 
         </Grid>
-        <Grid item xs={12} sm={4} ></Grid>
-        <Grid item xs={12} sm={4}  >
-            {/*             <PreviewCompatibleImage imageInfo={pic[2].image} style={{ borderRadius: 5 }} onMouseOver={() => setVisible(prev => !prev)} className={visible ? classes.imageMask : classes.imageNoMask} image={pic[2].image} fluid={pic[2].image.childImageSharp.fluid.src} /> */}
-            <img style={{ borderRadius: 5, width: 240 }} onMouseOver={() => setVisible(prev => !prev)} className={visible ? classes.imageMask : classes.imageNoMask} src={typeof pic[1].image === 'string' ? pic[1].image : pic[1].image} />
-            <p style={textWidth} className={visible ? classes.animatedItem : classes.animatedItemExiting}>{pic[1].text}</p>
-
-        </Grid>
-        <Grid item xs={12} sm={4} ></Grid>
 
 
-    </Grid >)
+
+    </Grid >)}else{return <p></p>}
 }
