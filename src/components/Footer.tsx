1d0cf8cf9e4d6c2f47b22a34bced5459711b895a
Laura <65158950+lauratro@users.noreply.github.com>
2021-08-13 13:53:07 +0200

    button in footer fixed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useRef, useState,} from 'react'
 import { Link } from 'gatsby'
+import ListIcon from '@material-ui/icons/List';;
 import SelectLanguage from '../SelectLanguage.tsx';
 import Button from '@material-ui/core/Button';
 
@@ -26,11 +27,40 @@ const useStyles = makeStyles((theme) => ({
    fontFamily:"Josefin Sans",
    fontSize:11
 
+  },
+  menu : {
+    background: "#ffffff",
+    borderRadius: 8,
+    position: "absolute",
+    top: 60,
+    left: 0,
+    width: 130,
+    opacity: 0,
+    visibility: "hidden",
+    transform: "translate(-100px, auto)",
+    transition:" opacity 0.4s ease, transform 0.4s ease, visibility 0.4s",
+    pointerEvents: "none",
+  },
+  menuActive: {
+    opacity: 1,
+    visibility: "visible",
+    transform: "translate(35px,-25px)", 
+    pointerEvents: "all",
+    zIndex: 100,
+    display:"flex",
+    flexDirection:"column"
   }
 }))
 
 export default function Footer({ langs, url, langKey }) {
   const classes = useStyles();
+  const dropdownRef = useRef(null);
+  const [isActive, setIsActive] = useState(false);
+
+  const onClick = () => {
+    setIsActive(!isActive);
+  }
+  console.log("active",isActive)
   const impressumLink =
   langKey === 'en' ? `/${langKey}/impressum` : '/impressum';
 const impressumText = langKey === 'en' ? 'Imprint' : 'Impressum';
@@ -63,20 +93,26 @@ const datenschutzText =
               </div>
             </div>
           <div style={{position:"absolute", left:5, display:"flex", flexDirection:"column",}}>
+          <ListIcon style={{ display: 'inline-block' }} onClick={onClick} />
+          <nav    ref={dropdownRef}
+          className={`${isActive ? 'menuActive' : 'menu'}`}
+          role="navigation"
+          aria-label="main-navigation">
           <Button
               to={impressumLink}
               component={Link}
-              style={{ textTransform: 'capitalize', margin:0, fontSize:10,padding:0 }}
+              style={{ textTransform: 'capitalize', marginRight:7, fontSize:12,padding:0, background:"white" }}
             >
               {impressumText}
             </Button>
             <Button
               to={datenschutzLink}
               component={Link}
-              style={{ textTransform: 'capitalize', margin:0, fontSize:10,padding:0 }}
+              style={{ textTransform: 'capitalize', margin:4, fontSize:12,padding:0, background:"white"  }}
             >
               {datenschutzText}
             </Button>
+            </nav>
           </div>
       </footer>
     )
