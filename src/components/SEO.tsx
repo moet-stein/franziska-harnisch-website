8178047c59f79b557d0c5c3dd76b213fd3dc751a
Moe Steinmueller <0101.mste@gmail.com>
2021-08-16 12:13:43 +0200

    SEO modified metaImage

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -21,9 +21,9 @@ function SEO({ description, lang, meta, image, title, location }) {
 
   const url = 'https://wizardly-shockley-75542d.netlify.app';
   // const metaDescription = description || site.siteMetadata.description
-  // const metaImage =
-  //     image ? `${url}/img/${image}`
-  //         : `${url}/${site.siteMetadata.ogImage}`
+  const metaImage = image
+    ? image
+    : `https://res.cloudinary.com/dcyfdwhvk/image/upload/c_scale,f_auto,q_100,w_2400/v1628759905/O-TON_im_blackegg_PollyFaber_Kulturnacht_HGW_14_09_2012_1_ceybfe.jpg`;
 
   return (
     <Helmet
@@ -75,13 +75,13 @@ function SEO({ description, lang, meta, image, title, location }) {
         },
         {
           name: `twitter:image`,
-          content: image,
+          content: metaImage,
         },
       ]
         .concat([
           {
             property: 'og:image',
-            content: image,
+            content: metaImage,
           },
           // {
           //     property: "og:image:width",
